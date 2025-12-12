3f9ba4601817ea969a6702312bfbf06110bed8c6
yiming-liao <yimingliao.official@gmail.com>
2025-12-13 00:23:12 +0700

    ci: add CI workflow for running tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     include: ["**/*.test.{ts,tsx}"],
+    coverage: { reporter: ["text", "lcov"] },
   },
   resolve: {
     alias: {
